123c6b5b1bf3b2b2c958d996ee85561cd318f63d
KushGabani <kushgabz2687@gmail.com>
2021-02-20 22:59:24 +0530

    Page loading animation added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Project from "./projects/project-page.js";
+import Loader from "./loading-animation/loader";
 import "./global-css/basics.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="app-wrapper">
+        <Loader />
         <Project />
       </div>
     );
