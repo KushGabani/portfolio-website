c73b65c60965af72389c09a494826c8c5419d3a5
KushGabani <kushgabz2687@gmail.com>
2021-11-04 11:05:43 +0530

    Changed Icons to fontawesome svg

--- src/units/Socials.js
+++ src/units/Socials.js
@@ -1,6 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-import { StaticImage } from "gatsby-plugin-image";
+import {
+    FaGithub,
+    FaInstagram,
+    FaLinkedin,
+    FaMediumM,
+    FaPinterest,
+    FaTwitter,
+} from "react-icons/fa";
+
+const Socials = () => (
+  <Social>
+    <SocialIcon>
+      <FaGithub />
+    </SocialIcon>
+    <SocialIcon>
+      <FaLinkedin />
+    </SocialIcon>
+    <SocialIcon>
+      <FaTwitter />
+    </SocialIcon>
+    <SocialIcon>
+      <FaMediumM />
+    </SocialIcon>
+    <SocialIcon>
+      <FaPinterest />
+    </SocialIcon>
+    <SocialIcon>
+      <FaInstagram />
+    </SocialIcon>
+  </Social>
+);
 
 const Social = styled.ol`
   display: flex;
@@ -8,6 +38,10 @@ const Social = styled.ol`
 `;
 const SocialIcon = styled.li`
   margin: 0 1rem;
+  svg {
+    width: 25px;
+    height: 25px;
+  }
 
   @media only screen and (min-width: 2000px) {
     margin: 0 1.5rem;
@@ -18,50 +52,4 @@ const SocialIcon = styled.li`
   }
 `;
 
-const Socials = () => (
-  <Social>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/Github.png"
-        alt={"Github"}
-      />
-    </SocialIcon>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/LinkedIn.png"
-        alt={"LinkedIn"}
-      />
-    </SocialIcon>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/Twitter.png"
-        alt={"Twitter"}
-      />
-    </SocialIcon>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/Medium.png"
-        alt={"Medium"}
-      />
-    </SocialIcon>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/Pinterest.png"
-        alt={"Pinterest"}
-      />
-    </SocialIcon>
-    <SocialIcon>
-      <StaticImage
-        className={"socials"}
-        src="./../assets/Instagram.png"
-        alt={"Instagram"}
-      />
-    </SocialIcon>
-  </Social>
-);
 export default Socials;
